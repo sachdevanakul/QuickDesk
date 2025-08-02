71d0c18243a95bf6da5052e52ebfc01572fa7d8a
nakul <sachdevanakul813@gmail.com>
2025-08-02 11:38:14 +0530

    Admin dashboard

---
+++ ADMINE/js/admin-script.js
@@ -0,0 +1,6 @@
+// ADMINE/js/admin-script.js
+document.addEventListener("DOMContentLoaded", () => {
+  // Later: Fetch data from backend
+  document.getElementById("userCount").textContent = "12";
+  document.getElementById("catCount").textContent = "5";
+});
